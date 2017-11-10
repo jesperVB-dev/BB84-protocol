c45c922ae8e5ba6fa79ba77aa240859b312de1b5
Jesper <jesper.vanbondt@icloud.com>
2017-11-10 23:53:48 +0100

    Added Eavesdropping detection
    it now detects if a person is evesdropping the channel.
    if yes it will try again

--- BB84-quantum-protocol.py
+++ BB84-quantum-protocol.py
@@ -1,67 +1,113 @@
 import random
-Qbits = 500
+Qbits = 2500
+Done = False
 
-ServerBits = []
-ServerEncrypters = []
-ServerPrivateKey = []
-ClientBits = []
-ClientDecrypters = []
-ClientPrivateKey = []
-CorrectBits = []
+def Keyexchange(Qbits):
+    CorrectBits = []
+    InCorrectBits = []
 
-#generate Random Server Bits
+    ServerBits = []
+    ServerEncrypters = []
+    ServerPrivateKey = []
+    ClientBits = []
+    ClientDecrypters = []
+    ClientPrivateKey = []
 
-for i in range(0,Qbits):
-    if random.randint(0, 1) == 0:
-        ServerEncrypters.append(0)
-    else:
-        ServerEncrypters.append(1)
-        
-    if random.randint(0, 1) == 0:
-        ServerBits.append(0)
-    else:
-        ServerBits.append(1)
+    EavesCheckServerBitsPrepare = []
+    EavesCheckServerBits = []
+    EavesCheckClientBitsPrepare = []
+    EavesCheckClientBits = []
 
-#
-#   Sends Qbits to client
-#
+    #generate Random Server Bits
 
-for i in range(0,Qbits):
-    if random.randint(0, 1) == 0:
-        ClientDecrypters.append(0)
-    else:
-        ClientDecrypters.append(1)
-        
-    if random.randint(0, 1) == 0:
-        if ServerEncrypters[i] == ClientDecrypters[i]:
-            ClientBits.append(ServerBits[i])
+    for i in range(0,Qbits):
+        if random.randint(0, 1) == 0:
+            ServerEncrypters.append(0)
         else:
-            ClientBits.append(random.randint(0, 1))
-    else:
-        if ServerEncrypters[i] == ClientDecrypters[i]:
-            ClientBits.append(ServerBits[i])
+            ServerEncrypters.append(1)
+            
+        if random.randint(0, 1) == 0:
+            ServerBits.append(0)
         else:
-            ClientBits.append(random.randint(0, 1))
+            ServerBits.append(1)
 
-#
-#   Send ClientDecrypters to Server
-#
+    #
+    #   Sends Qbits to client
+    #
 
-for i in range(0, len(ServerEncrypters)):
-    if ServerEncrypters[i] == ClientDecrypters[i]:
-        ServerPrivateKey.append(ServerBits[i])
-        CorrectBits.append(1)
-    else:
-        CorrectBits.append(0)        
+    for i in range(0,Qbits):
+        if random.randint(0, 1) == 0:
+            ClientDecrypters.append(0)
+        else:
+            ClientDecrypters.append(1)
+            
+        if random.randint(0, 1) == 0:
+            if ServerEncrypters[i] == ClientDecrypters[i]:
+                ClientBits.append(ServerBits[i])
+            else:
+                ClientBits.append(random.randint(0, 1))
+        else:
+            if ServerEncrypters[i] == ClientDecrypters[i]:
+                ClientBits.append(ServerBits[i])
+            else:
+                ClientBits.append(random.randint(0, 1))
 
-#
-#   Send CorrectBits from Server to Client
-#
-for i in range(0, len(CorrectBits)):
-    if CorrectBits[i] == 1:
-        ClientPrivateKey.append(ClientBits[i])
+    #
+    #   Send ClientDecrypters to Server
+    #
 
-print(ServerPrivateKey)
-print(ClientPrivateKey)
+    for i in range(0, len(ServerEncrypters)):
+        if ServerEncrypters[i] == ClientDecrypters[i]:
+            ServerPrivateKey.append(ServerBits[i])
+            CorrectBits.append(1)
+        else:
+            CorrectBits.append(0)
+            InCorrectBits.append(i)
 
+    #
+    #   Send CorrectBits from Server to Client
+    #
 
+    for i in range(0, len(CorrectBits)):
+        if CorrectBits[i] == 1:
+            ClientPrivateKey.append(ClientBits[i])
+        else:
+            ClientDecrypters[i] = (ClientDecrypters[i] + 1) % 2
+
+    #
+    #   Check for Eavesdroppers from the Servers side
+    #
+
+    for i in range(0, len(InCorrectBits)):
+        EavesCheckServerBitsPrepare.append(InCorrectBits[i])
+        
+    for i in range(0, len(EavesCheckServerBitsPrepare)):
+        EavesCheckServerBits.append((ServerPrivateKey[EavesCheckServerBitsPrepare[i]%len(ServerPrivateKey)] + 1) % 2)
+        
+    #
+    #   Send InCorrectBits to Client
+    #
+
+    for i in range(0, len(InCorrectBits)):
+        EavesCheckClientBitsPrepare.append(InCorrectBits[i])
+        
+    for i in range(0, len(EavesCheckClientBitsPrepare)):
+        EavesCheckClientBits.append((ClientPrivateKey[EavesCheckServerBitsPrepare[i]%len(ClientPrivateKey)] + 1) % 2)
+
+    #
+    #   Send EavesCheckClientBits to Server
+    #
+
+    return EavesCheckClientBits, EavesCheckServerBits
+
+def main(Done):
+    while Done != True:
+        EavesCheckClientBits, EavesCheckServerBits = Keyexchange(Qbits)
+        if (EavesCheckClientBits == EavesCheckServerBits):
+            print("Key exchange complete!")
+            Done = True
+        else:
+            print("Eavesdropper! Trying again.")
+        
+if __name__ == "__main__":
+    main(Done)
